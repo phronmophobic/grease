45d0d7c816cdba89d13c59272a5d671d3f0deed5
Adrian Smith <smith.adriane@gmail.com>
2024-09-19 22:25:49 -0700

    Update accelerometer example.

--- examples/app/accelerometer.clj
+++ examples/app/accelerometer.clj
@@ -18,12 +18,7 @@
    {:name :y :datatype :float64}
    {:name :z :datatype :float64}])
 
-(objc ^void
-      [motion :startAccelerometerUpdatesToQueue:withHandler
-       main-queue
-       (fn ^void [data error]
-         (let [acceleration (objc ^cm_accleration [data :acceleration])]
-           (swap! accelerations conj acceleration)))])
+
 
 (def scale 100)
 
@@ -32,27 +27,47 @@
     x
     (- x)))
 
-(defn show-accelerations [accelerations]
-  (apply
-   ui/vertical-layout
-   (for [{:keys [x y z]} (take-last 90 accelerations)]
-     (let [body (apply
-                 ui/horizontal-layout
-                 (for [[a color] [[x [1 0 0]]
-                                  [y [0 1 0]]
-                                  [z [0 0 1]]]]
-                   (ui/filled-rectangle color
-                                        (* (abs a) scale) 5)))
-           [w h] (ui/bounds body)]
-       body))))
+(defn show-accelerations [accs]
+  (ui/vertical-layout
+   (ui/horizontal-layout
+    (ui/button
+     "start"
+     (fn []
+       (objc ^void
+             [motion :startAccelerometerUpdatesToQueue:withHandler
+              main-queue
+              (fn ^void [data error]
+                (let [acceleration (objc ^cm_accleration [data :acceleration])]
+                  (swap! accelerations conj acceleration)))])
+       nil))
+    (ui/spacer 50 0)
+    (ui/button
+     "stop"
+     (fn []
+       (objc ^void [motion :stopAccelerometerUpdates]))))
+   (apply
+    ui/vertical-layout
+    (for [{:keys [x y z]} (take-last 90 accs)]
+      (let [body (apply
+                  ui/horizontal-layout
+                  (for [[a color] [[x [1 0 0]]
+                                   [y [0 1 0]]
+                                   [z [0 0 1]]]]
+                    (ui/filled-rectangle color
+                                         (* (abs a) scale) 5)))
+            [w h] (ui/bounds body)]
+        body)))))
+
+(defn repaint! []
+  (reset! main-view
+          (ui/translate 25 100
+                        (show-accelerations @accelerations))))
 
 (add-watch accelerations ::update-view
            (fn [_ _ old accelerations]
-             (reset! main-view
-                     (ui/translate 25 100
-                                   (show-accelerations accelerations)))))
+             (repaint!)))
 
-(comment
-  (objc
-   [motion :stopAccelerometerUpdates])
-  ,)
+(defonce __init
+  (repaint!))
+
+
