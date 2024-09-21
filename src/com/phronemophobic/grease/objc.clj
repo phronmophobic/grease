b133094dd30da6d9be91cc408cb9dee304d5ae5b
Adrian Smith <smith.adriane@gmail.com>
2024-09-21 12:38:32 -0700

    Starting adding helpers to precompiled namespace.

--- src/com/phronemophobic/grease/objc.clj
+++ src/com/phronemophobic/grease/objc.clj
@@ -2,7 +2,10 @@
   (:require [tech.v3.datatype.ffi :as dt-ffi]
             [tech.v3.datatype :as dtype]
             tech.v3.datatype.ffi.graalvm-runtime
-            [tech.v3.datatype.native-buffer :as native-buffer])
+            [com.phronemophobic.clj-libffi :as ffi]
+            [com.phronemophobic.objcjure :refer [objc] :as objc]
+            [tech.v3.datatype.native-buffer :as native-buffer]
+            [clojure.java.io :as io])
   (:gen-class))
 
 ;; https://developer.apple.com/documentation/objectivec/objective-c_runtime?language=objc
@@ -21,7 +24,7 @@
 
 
 (dt-ffi/define-library-interface
- {;; :objc_msgSend {:rettype :int64
+  {;; :objc_msgSend {:rettype :int64
    ;;                :argtypes [['obj :pointer]
    ;;                           ['sel :pointer]]}
 
@@ -42,14 +45,14 @@
 
    ;; :call_clj_fn {:rettype :void
    ;;               :argtypes [['fptr :pointer]]}
-  :clj_main_view {:rettype :pointer
-                  :argtypes []}
+   :clj_main_view {:rettype :pointer
+                   :argtypes []}
 
-  :clj_app_dir {:rettype :pointer
-                :argtypes []}
+   :clj_app_dir {:rettype :pointer
+                 :argtypes []}
 
-  :clj_debug {:rettype :pointer
-              :argtypes [['data :pointer]]}
+   :clj_debug {:rettype :pointer
+               :argtypes [['data :pointer]]}
    ,})
 
 (comment
@@ -64,3 +67,70 @@
       )))
 
 
+
+(def UIAlertControllerStyleActionSheet 0)
+(def UIAlertControllerStyleAlert 1)
+
+(def UIAlertActionStyleDefault 0)
+(def UIAlertActionStyleCancel 1)
+(def UIAlertActionStyleDestructive 2)
+
+(def main-queue (delay (ffi/dlsym ffi/RTLD_DEFAULT (dt-ffi/string->c "_dispatch_main_q"))))
+
+(defn NSSetUncaughtExceptionHandler [handler]
+  (ffi/call "NSSetUncaughtExceptionHandler" :void :pointer handler)
+  )
+
+(defn dispatch-main-async [f]
+  (ffi/call "dispatch_async" :void :pointer @main-queue :pointer (objc
+                                                                  (fn ^void []
+                                                                    (try
+                                                                      (f)
+                                                                      (catch Exception e
+                                                                        (println e)))))))
+
+#_(defn show-alert! []
+    (dispatch-main-async
+     (fn []
+       (let [vc (objc [[[UIApplication :sharedApplication] :keyWindow] :rootViewController])
+             alert (objc
+                    [UIAlertController :alertControllerWithTitle:message:preferredStyle
+                     @"Title"
+                     @"Message"
+                     UIAlertControllerStyleAlert])
+             default-action (objc [UIAlertAction :actionWithTitle:style:handler
+                                   @"default"
+                                   UIAlertActionStyleDefault
+                                   (fn ^void [action]
+                                     (log :default-action))])]
+         (objc [alert :addAction default-action])
+         (objc [vc :presentViewController:animated:completion
+                alert true nil])))))
+
+(defn show-keyboard []
+  (objc ^void
+        [~(clj_main_view) :performSelectorOnMainThread:withObject:waitUntilDone
+         ~(objc/sel_registerName (dt-ffi/string->c "becomeFirstResponder"))
+         nil
+         ~(byte 0)]))
+
+(defn hide-keyboard []
+  (objc ^void
+        [~(clj_main_view) :performSelectorOnMainThread:withObject:waitUntilDone
+         ~(objc/sel_registerName (dt-ffi/string->c "resignFirstResponder"))
+         nil
+         ~(byte 0)]))
+
+
+(defn documents-dir []
+  ;; fileSystemRepresentation
+  (io/file
+   (dt-ffi/c->string
+    (objc [[[[NSFileManager defaultManager] :URLsForDirectory:inDomains
+             ;; (int 14) ;; application support
+             (int 9) ;; documents
+             (int 1)
+             ]
+            :objectAtIndex 0]
+           fileSystemRepresentation])))
+  )
