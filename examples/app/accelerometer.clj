e95ac7cdeb71c19ca72c65af4dfae3c912cb5d2a
Adrian Smith <smith.adriane@gmail.com>
2026-05-08 11:20:24 +0200

    fix accelerator example.

--- examples/app/accelerometer.clj
+++ examples/app/accelerometer.clj
@@ -1,14 +1,29 @@
 (ns accelerometer
   (:require [membrane.ui :as ui]
+            [membrane.component
+             :refer [defui defeffect]]
+            app
             [tech.v3.datatype.struct :as dt-struct]
             [com.phronemophobic.objcjure :refer [objc describe]
              :as objc]))
+
+(defonce log* (atom []))
+(defn log [& msgs]
+  (swap! log* into msgs)
+  nil)
+
+
+(defn push [xs x n]
+  (let [accelerations (conj xs x)]
+    ;; should be using a queue
+    (subvec accelerations (max 0 (- (count accelerations)
+                                    n)))))
 
 (def motion (objc [[CMMotionManager :alloc] :init]))
 
 (objc ^void [motion :setAccelerometerUpdateInterval 0.05])
 
-(def accelerations (atom []))
+(defonce accelerations (atom []))
 
 (def main-queue (objc [NSOperationQueue :mainQueue]))
 
@@ -27,24 +42,41 @@
     x
     (- x)))
 
+
+(defn button [text on-click]
+  (ui/on
+   :mouse-down
+   (fn [_]
+     (when on-click
+       (on-click)))
+   (ui/bordered
+    [20 20]
+    (ui/label text (ui/font nil 33)))))
+
+(defonce app-state (atom {:accelerations []}))
+
 (defn show-accelerations [accs]
   (ui/vertical-layout
-   (ui/horizontal-layout
-    (ui/button
-     "start"
-     (fn []
-       (objc ^void
-             [motion :startAccelerometerUpdatesToQueue:withHandler
-              main-queue
-              (fn ^void [data error]
-                (let [acceleration (objc ^cm_accleration [data :acceleration])]
-                  (swap! accelerations conj acceleration)))])
-       nil))
-    (ui/spacer 50 0)
-    (ui/button
-     "stop"
-     (fn []
-       (objc ^void [motion :stopAccelerometerUpdates]))))
+   (ui/translate
+    10 100
+    (ui/horizontal-layout
+     (button
+      "start"
+      (fn []
+        (objc ^void
+              [motion :startAccelerometerUpdatesToQueue:withHandler
+               main-queue
+               (fn ^void [data error]
+                 (let [acceleration (objc ^cm_accleration [data :acceleration])]
+                   (swap! app-state update :accelerations
+                          push acceleration 100)))])
+        nil))
+     (ui/spacer 50 0)
+     (button
+      "stop"
+      (fn []
+        (objc ^void [motion :stopAccelerometerUpdates])
+        nil))))
    (apply
     ui/vertical-layout
     (for [{:keys [x y z]} (take-last 90 accs)]
@@ -58,16 +90,31 @@
             [w h] (ui/bounds body)]
         body)))))
 
-(defn repaint! []
-  (reset! main-view
-          (ui/translate 25 100
-                        (show-accelerations @accelerations))))
 
-(add-watch accelerations ::update-view
-           (fn [_ _ old accelerations]
+
+(defn viewfn []
+  (show-accelerations (:accelerations @app-state)))
+
+
+
+(defn repaint! []
+  (when-let [repaint! (:repaint! @app-state)]
+    (repaint!)))
+
+(defn -main []
+  (let [{:keys [repaint!]}
+        (app/show! {:on-close (fn []
+                                (swap! app-state dissoc :repaint!))
+                    :view-fn viewfn})]
+    (swap! app-state assoc
+           :repaint! repaint!
+           :accelerations [])
+    (repaint!)))
+
+(add-watch app-state ::update-view
+           (fn [_ _ _ _]
              (repaint!)))
 
-(defonce __init
-  (repaint!))
+
 
 
